23c1a3784602ae2e1054b07d35f4ce35708bb64b
Christoph Petschnig <christoph.petschnig@infopark.de>
2011-02-26 21:02:20 +0100

    New Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkshopManager::Application.config.secret_token = '36001e80b6a80705a5a69fd87756a1e184765488770c45b3f5f3a5c91068c64839ce884a5fc9c49bb2464c202e665d98683d82cbb00b9224abf5b80b050a13a4'
