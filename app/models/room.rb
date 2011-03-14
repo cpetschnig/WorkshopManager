77a66c8fb52853f920b85b635cb9031b29681092
Christoph Petschnig <christoph.petschnig@infopark.de>
2011-03-14 08:40:57 +0100

    Check, if room is available

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,18 @@ class Room < ActiveRecord::Base
 
   has_many :workshops
 
+  def available?(from, to, exclude_workshops = [])
+    exclude_workshops = [exclude_workshops] unless exclude_workshops.is_a?(Array)   # convert last parameter to Array
+    exclude_ids = exclude_workshops.map{|workshop| workshop.id}
+    exclude_ids = [-1] if exclude_ids.empty?
+
+    # too bad we cannot do this: there is no colum `end_at'
+    # Workshop.count(:conditions => ["room_id = ? AND start_at < ? AND end_at > ? AND id NOT IN ?", self.id, to, from, exclude_ids]) == 0
+
+    # let's help ourselves with this ugly solution
+    Workshop.all(:conditions => ["room_id = ? AND start_at < ? AND id NOT IN (?)",
+        self.id, to, exclude_ids]).select do |workshop|
+      workshop.end_at > from
+    end.count == 0
+  end
 end
--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -5,9 +5,15 @@ class Workshop < ActiveRecord::Base
   validates :name, :length => {:maximum => 20, :minimum => 3}
   validates :room_id, :presence => true
 
+  validate :room_availability_validation
+
   def end_at
     return unless self[:start_at]
     self[:start_at] + (self[:duration] || 0).minutes
+  end
+
+  def room_availability_validation
+    errors.add(:base, I18n.translate('errors.room_booked')) unless room.available?(self.start_at, self.end_at, self)
   end
 
 end
